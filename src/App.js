c9e8e1d3975121515469d3cce211301fc5b36cac
Francelli Nakamura <franci.nakamura100@gmail.com>
2020-11-23 23:34:23 -0800

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App() { 
+  return(
+    <div className="App-header">
+      <h1>My Ticking Clock</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
     </div>
-  );
+  )
 }
 
 export default App;
